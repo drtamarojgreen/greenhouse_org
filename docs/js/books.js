2054b46a0a97cb6a809bcc6d6706d809d4c8ace7
Tamaro Green <tamarojgreen@gmail.com>
2025-09-09 10:35:19 -0600

    updating books

--- docs/js/books.js
+++ docs/js/books.js
@@ -192,7 +192,7 @@
 
                 try {
                     // Use a relative path for the API call, assuming the server.js handles the routing
-                    const response = await fetch('/_functions/getBooks'); 
+                    const response = await fetch('/_functions/get_getBooks'); 
                     if (!response.ok) {
                         throw new Error(`HTTP error! status: ${response.status}`);
                     }
